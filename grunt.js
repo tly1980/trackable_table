ab833cde009f370663b5689c68c3390f4f94ebae
Tom Tang <tly1980@gmail.com>
2012-10-30 15:56:57 +1100

    fix the grunt.js and npm-shrinkwrap added

--- grunt.js
+++ grunt.js
@@ -10,20 +10,47 @@ module.exports = function(grunt) {
     },
 
     shell: {
-      copy: {
-        command: 'cp -R trackable_table/ demos/demo1/js/apps/trackable_table'
+      // copy: {
+      //   command: 'cp -R trackable_table/ demos/demo1/js/apps/trackable_table'
+      // },
+      sass: {
+          command: 'sass --update assets/css'
+      },
+      copy_js: {
+          command: 'cp -R trackable_table/ demos/demo1/js/apps/trackable_table'
+      },
+      copy_css: {
+          command: 'cp assets/css/*.css demos/demo1/css/trackable_table/.'
       }
     },
 
     watch: {
-      files: '<config:html2json.dist.src>',
-      tasks: 'default'
+      sass: {
+          files: ['assets/css/*.scss', 'assets/css/*.sass'],
+          tasks: ['shell:sass']
+      },
+
+      watch_css: {
+          files: ['assets/css/*.css'],
+          tasks: ['shell:copy_css']
+      },
+
+      watch_tpl: {
+          files: ['<config:html2json.dist.src>'],
+          tasks: ['html2json', 'shell:copy_js']
+      },
+
+      watch_js: {
+          files: ['trackable_table/*.js'],
+          tasks: ['shell:copy_js']
+      }
     }
   });
 
   // Load local tasks.
   grunt.loadNpmTasks('grunt-html2json');
   grunt.loadNpmTasks('grunt-shell');
+  //grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
   grunt.registerTask('default', 'html2json shell');
