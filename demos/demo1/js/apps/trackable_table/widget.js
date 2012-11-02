e1c6822898ae5d62911fb8168f07cc5a0a12df17
Tom Tang <tly1980@gmail.com>
2012-11-02 18:25:28 +1100

    misc changes

--- demos/demo1/js/apps/trackable_table/widget.js
+++ demos/demo1/js/apps/trackable_table/widget.js
@@ -3,14 +3,7 @@ define(['jquery',
         'backbone',
         'mustache',
         'text!./templates/tpl.json'],
-    function ($, _, Backbone, mustache, tpl) {
-
-    // var _headers = Backbone.Collection([
-    //     { text: 'Header 1', key: 'header1'},
-    //     { text: 'Header 2', key: 'header2'},
-    //     { text: 'Header 3', key: 'header3'},
-    //     { text: 'Header 4', key: 'header4'}
-    // ]);
+    function ($, _, Backbone, Mustache, tpl) {
 
     var _headers = {
         header1: 'Header 1',
@@ -19,35 +12,65 @@ define(['jquery',
         header4: 'Header 4'
     };
 
-    var _rows = new Backbone.Collection([
-        {
-            header1: 'value a',
-            header2: 'value b',
-            header3: 'value c',
-            header4: 'value d'
-        },
+    var _header_cfg ;
 
-        {
-            header1: 'value a',
-            header2: 'value b',
-            header3: 'value c',
-            header4: 'value d'
-        },
 
-        {
-            header1: 'value a',
-            header2: 'value b',
-            header3: 'value c',
-            header4: 'value d'
-        },
 
-        {
-            header1: 'value a',
-            header2: 'value b',
-            header3: 'value c',
-            header4: 'value d'
-        }
+    var _changes = new Backbone.Collection([
     ]);
+
+    function header_defaultcfg(headers){
+        _.each(headers, function(value, key){
+            headers[key] =  $.extend({
+                text: 'Un-Titled',
+                tpl: '{{val}}',
+                changeable: false,
+                extra_cls: []
+            }, value);
+
+            if (headers[key].changeable === true ){
+                headers[key].extra_cls.push('changeable');
+            }
+        });
+    }
+
+    var InputView = Backbone.View.extend({
+
+        events: {
+            "keyup input": "input_keyup"
+        },
+
+        input_keyup: function(event){
+            if (event.keyCode === 27 ){
+                this.freeze_value(
+                    this.model.get('origin_data'));
+            }
+            console.log('event', event);
+        },
+
+        recover: function(){
+        },
+
+        freeze_value: function(value){
+            console.log('freeze_value, model', this.model.toJSON());
+            var html = Mustache.render(
+                this.model.get('tpl'),
+                { val: this.model.get('origin_data') });
+            this.$el.replaceWith(html);
+        },
+
+        render: function(){
+            var html = '<input value="' + this.model.get('origin_data') + '"/>';
+            this.$el.html(html);
+            return this;
+        }
+    });
+
+    function origin_val($elem){
+        var cid = $elem.parent().attr('cid');
+        return this.collection.getByCid(cid);
+        //_rows[]
+    }
 
     var DataTable = Backbone.View.extend({
 
@@ -55,19 +78,37 @@ define(['jquery',
         className: "trackable_dt_table",
 
         events: {
-            "dblclick td":   "dblclick_td"
+            "dblclick td.changeable":   "dblclick_td"
         },
 
         dblclick_td: function(event){
+            var $target_elem = $(event.currentTarget);
+            var cid = $target_elem.parent().attr('cid');
+            var field = $target_elem.attr('field');
+            var row_model = this.collection.getByCid(cid);
+            var tpl = _headers[field].tpl;
+            
+            var change = new Backbone.Model({
+              origin_data: row_model.get(field),
+              field: field,
+              tpl: tpl
+            });
 
+            var input = new InputView({
+                model: change
+            });
+            //$target_elem.empty();
+            $target_elem.html(input.render().$el);
+            input.$('input').focus();
+            //console.log('change', change.toJSON());
         },
 
         initialize: function(){
-            _rows.bind('reset',  this.render, this);
+            this.collection.bind('reset',  this.render, this);
         },
 
         render: function() {
-            var tbl_html = '<table>' + this.render_thead() + this.render_tbody() + '</tbody>';
+            var tbl_html = '<table class="table">' + this.render_thead() + this.render_tbody() + '</tbody>';
             this.$el.html(tbl_html);
             return this;
         },
@@ -75,7 +116,7 @@ define(['jquery',
         render_thead: function(){
             var html = '<thead>';
             _.each(_headers, function(value, key){
-                html += '<th>' + value +' </th>';
+                html += '<th>' + value.text +' </th>';
             });
 
             html += '</thead>';
@@ -83,16 +124,20 @@ define(['jquery',
         },
 
         render_tbody: function(){
-            rows = _rows.toJSON();
             var html = '<tbody>';  //tbody starts
-            _.each( rows, function(r){
-                html += '<tr>'; // tr starts
+            this.collection.each( function(r, i){
+                html += '<tr cid="' + r.cid + '" >'; // tr starts
                 _.each(_headers, function(value, key){
-                    html += '<td>'; // td
+
+                    html += '<td field="' + key + '" class="' +
+                            value.extra_cls.join(',') + '">'; // td
                     //console.log('key:', key, ' value:', value);
                     //console.log('r', r);
 
-                    html += r[key];
+                    html += Mustache.render(
+                        value.tpl,
+                        {val: r.get(key)});
+
                     html += '</td>';
                 });
                 html += '</tr>';  // tr ends
@@ -104,22 +149,20 @@ define(['jquery',
     });
 
     return {
-        datatable: new DataTable(),
-
-        init:function(){
+        
+        init:function(columns, new_url){
+            this.rows = new (Backbone.Collection.extend({
+                url: new_url
+            }))();
+            this.datatable = new DataTable({collection: this.rows});
+            _headers = columns;
+            header_defaultcfg(_headers);
+            //this.rows.url = new_url;
             console.log('trackable_table widget init');
         },
 
-        url: function(new_url){
-           _rows.url = new_url;
-        },
-
-        colums: function(header){
-            _headers = header;
-        },
-
         fetch: function(){
-            _rows.fetch();
+            this.rows.fetch();
         }
 
     };
--- demos/demo1/js/bootup.js
+++ demos/demo1/js/bootup.js
@@ -18,16 +18,15 @@ requirejs.config({
 // Start loading the main app file. Put all of
 // your application logic in there.
 requirejs(['apps/trackable_table/widget'], function(widget){
-    widget.init();
-    widget.url('/res/product_info');
-    $('div.dt_placeholder').append(
-        widget.datatable.$el.addClass('table'));
+    widget.init({
+        name:         {text: 'Product Name', changeable:true},
+        unit_price:   {text: 'Unit Price', tpl: '${{val}}'},
+        buying_price: {text: 'Buying Price', tpl: '${{val}}', changeable: true},
+        sold_count:   {text: 'Sold Count', changeable:true}
+    }, '/res/product_info');
 
-    widget.colums({
-        name: 'Product Name',
-        unit_price: 'Unit Price',
-        buying_price: 'Buying Price',
-        sold_count: 'Sold Count'
-    });
+    $('div.dt_placeholder').append(
+        widget.datatable.$el);
+
     widget.fetch();
 });
